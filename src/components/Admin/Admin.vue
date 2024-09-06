61b53d556b42bcebafbf5d1aa84b447f6d7f2c01
Nickolay <kolya.usmanskiy@gmail.com>
2024-09-06 22:23:44 +0300

    Save

--- src/components/Admin/Admin.vue
+++ src/components/Admin/Admin.vue
@@ -28,65 +28,65 @@ const logout = async () => {
 </script>
 
 <style scoped>
-.container {
-  display: flex;
-  flex-direction: column;
-  /* align-items: center;
-  justify-content: center; */
-  min-height: 100vh;
-  background-color: #f2f2f2;
-  padding: 20px;
-}
+  .container {
+    display: flex;
+    flex-direction: column;
+    /* align-items: center;
+    justify-content: center; */
+    min-height: 100vh;
+    background-color: #f2f2f2;
+    padding: 20px;
+  }
 
-.user-info {
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  text-align: center;
-  margin-bottom: 20px;
-}
+  .user-info {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    text-align: center;
+    margin-bottom: 20px;
+  }
 
-.user-info p {
-  font-size: 18px;
-  color: #333;
-  margin-bottom: 10px;
-}
+  .user-info p {
+    font-size: 18px;
+    color: #333;
+    margin-bottom: 10px;
+  }
 
-.logout-button {
-  padding: 10px 20px;
-  background-color: #ff4d4d;
-  color: white;
-  border: none;
-  border-radius: 4px;
-  cursor: pointer;
-  font-size: 16px;
-}
+  .logout-button {
+    padding: 10px 20px;
+    background-color: #ff4d4d;
+    color: white;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+    font-size: 16px;
+  }
 
-.logout-button:hover {
-  background-color: #e60000;
-}
+  .logout-button:hover {
+    background-color: #e60000;
+  }
 
-.login-component {
-  max-width: 400px;
-  margin: 0 auto;
-  border: 1px solid #ddd;
-  border-radius: 8px;
-  background-color: #fff;
-  padding: 20px;
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-}
+  .login-component {
+    max-width: 400px;
+    margin: 0 auto;
+    border: 1px solid #ddd;
+    border-radius: 8px;
+    background-color: #fff;
+    padding: 20px;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  }
 
-.active-elements {
-  max-width: 600px;
-  width: 100%;
-  background-color: #fff;
-  border-radius: 8px;
-  padding: 20px;
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-}
+  .active-elements {
+    max-width: 600px;
+    width: 100%;
+    background-color: #fff;
+    border-radius: 8px;
+    padding: 20px;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  }
 
-.information-page {
-  display: grid;
-  gap: 1%;
-}
+  .information-page {
+    display: grid;
+    gap: 1%;
+  }
 </style>
